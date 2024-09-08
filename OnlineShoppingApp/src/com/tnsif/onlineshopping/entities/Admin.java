979ebbad6225a53225477d091cd7aca703f91a6c
fazaulfath <149148613+fazaulfath@users.noreply.github.com>
2024-09-08 19:59:00 +0530

    second commit

--- OnlineShoppingApp/src/com/tnsif/onlineshopping/entities/Admin.java
+++ OnlineShoppingApp/src/com/tnsif/onlineshopping/entities/Admin.java
@@ -1,5 +1,8 @@
+//Program to define the admin entity
 package com.tnsif.onlineshopping.entities;
 
-public class Admin {
-
+public class Admin extends User {
+	public Admin (int userId, String username, String email) {
+		super (userId, username, email) ;
+	}
 }
