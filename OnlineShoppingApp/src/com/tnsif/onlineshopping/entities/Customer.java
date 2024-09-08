f9d44de4df231dea6fece61975d813b70a782a31
fazaulfath <149148613+fazaulfath@users.noreply.github.com>
2024-09-08 20:17:37 +0530

    third commit

--- OnlineShoppingApp/src/com/tnsif/onlineshopping/entities/Customer.java
+++ OnlineShoppingApp/src/com/tnsif/onlineshopping/entities/Customer.java
@@ -1,5 +1,39 @@
+//Program to define Customer entity
 package com.tnsif.onlineshopping.entities;
 
-public class Customer {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Customer extends User{
+	private String address;
+	private ShoppingCart shoppingCart;
+	private List<Order> orders;
+
+	public Customer (int userId, String username, String email, String address) {
+		super(userId, username, email) ;
+		this.address = address;
+		this.shoppingCart = new ShoppingCart ();
+		this.orders = new ArrayList<>();
+	}
+
+	//getters and setters
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public ShoppingCart getShoppingCart() {
+		return shoppingCart;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
+	}
+
+	public void addOrder (Order order) {
+		orders.add (order);
+	}
 }
